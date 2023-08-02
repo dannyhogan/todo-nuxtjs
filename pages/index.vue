b62f93e951a0d9c11dfcfcb1b9fe23c936f29590
Danny Hogan <dhogan@esha.com>
2023-08-02 12:54:31 -0400

    UI components for todo list

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    <TodoList />
+  </Layout>
+</template>
+
+<script>
+import Layout from "../components/Layout.vue";
+import TodoList from "../components/ToDoList.vue";
+
+export default {
+  name: "IndexPage",
+  components: { Layout, TodoList },
+};
+</script>
